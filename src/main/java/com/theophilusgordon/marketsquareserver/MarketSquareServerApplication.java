c174668ee8ad1b2f09f7aa85720516c3ec5e3fc1
Theophilus Gordon <theophilusgordon@yahoo.com>
2023-02-06 08:49:24 +0000

    feat: initial commit

---
+++ src/main/java/com/theophilusgordon/marketsquareserver/MarketSquareServerApplication.java
@@ -0,0 +1,13 @@
+package com.theophilusgordon.marketsquareserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSquareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketSquareServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theophilusgordon/marketsquareserver/MarketSquareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theophilusgordon.marketsquareserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSquareServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
