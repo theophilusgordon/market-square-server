f4d50f559bbb16bd285c95abcf41a58c19a2f9f9
Theophilus Gordon <theophilusgordon@yahoo.com>
2023-05-09 13:01:45 +0000

    refactor: removed unused imports

--- src/main/java/com/theophilusgordon/marketsquareserver/service/FileStorageService.java
+++ src/main/java/com/theophilusgordon/marketsquareserver/service/FileStorageService.java
@@ -2,11 +2,6 @@ package com.theophilusgordon.marketsquareserver.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public interface FileStorageService {
     public String uploadFile(MultipartFile file);
 }
