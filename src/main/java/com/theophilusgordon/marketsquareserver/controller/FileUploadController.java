65ff21c51cae2cb76aa69290a4aacbfd2cc8178b
Theophilus Gordon <theophilusgordon@yahoo.com>
2023-05-12 16:41:16 +0000

    refactor: return file url instead of file name on upload

--- src/main/java/com/theophilusgordon/marketsquareserver/controller/FileUploadController.java
+++ src/main/java/com/theophilusgordon/marketsquareserver/controller/FileUploadController.java
@@ -1,5 +1,7 @@
 package com.theophilusgordon.marketsquareserver.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.theophilusgordon.marketsquareserver.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URL;
+import java.util.Date;
+
 @RestController
 @RequestMapping("/api/v1/files")
 public class FileUploadController {
@@ -17,9 +22,15 @@ public class FileUploadController {
     @Autowired
     private FileStorageService fileStorageService;
 
+    @Autowired
+    private AmazonS3 amazonS3Client;
+
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         String fileName = fileStorageService.uploadFile(file);
-        return new ResponseEntity<>("File uploaded successfully: " + fileName, HttpStatus.OK);
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest("market-sqaure-storage", fileName);
+        generatePresignedUrlRequest.setExpiration(new Date(System.currentTimeMillis() + 3600000));
+        URL fileUrl = amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return new ResponseEntity<>("File uploaded successfully: " + fileUrl, HttpStatus.OK);
     }
 }
