8edce492da499dde1235c1ac2a12b4a10b59b345
Theophilus Gordon <theophilusgordon@yahoo.com>
2023-05-15 13:11:40 +0000

    refactor: jwt error to global exception handler

--- src/main/java/com/theophilusgordon/marketsquareserver/advice/GlobalExceptionHandler.java
+++ src/main/java/com/theophilusgordon/marketsquareserver/advice/GlobalExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.theophilusgordon.marketsquareserver.advice;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ValidationExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -24,6 +26,14 @@ public class ValidationExceptionHandler {
         );
 
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation Error", errors);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ErrorResponse handleExpiredJwtException(ExpiredJwtException ex) {
+        Map<String, String> errors = new LinkedHashMap<>();
+        errors.put("token", "Token has expired. Please login and try again");
+        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "Unauthorized", errors);
     }
 
     @Getter
